e2235c61ca73ed096a33b12881e72c87098cf740
ZenithSlayer <miguellurk+github@gmail.com>
2026-09-09 11:36:49 -0300

    Chore: added missing theme styling

--- src/app/product.jsx
+++ src/app/product.jsx
@@ -80,7 +80,7 @@ const product = ({ id, onAddToCart, setToast }) => {
             <Text style={{ fontSize: 18, fontWeight: "bold" }}>-</Text>
           </Pressable>
 
-          <Text style={{ marginHorizontal: 20, fontSize: 18, fontWeight: "600" }}>{quantity}</Text>
+          <Text style={{ marginHorizontal: 20, fontSize: 18, fontWeight: "600", color: theme.text }}>{quantity}</Text>
 
           <Pressable
             onPress={() => setQuantity((q) => q + 1)}
